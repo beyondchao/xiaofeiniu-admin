a793ac34a191c8f2e21b544629bc6f5e8399a179
YChao <yc12877782@gmail.com>
2019-02-14 14:48:10 +0800

    commit

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,15 +3,15 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+require.context('../src/assets/img/dish', false, /\.jpg$/)
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
 
-
 module.exports = {
-  context: path.resolve(__dirname, '../'),
+  context: path.resolve('./src'),
   entry: {
     app: './src/main.js'
   },
--- src/components/Security.vue
+++ src/components/Security.vue
@@ -8,7 +8,7 @@
             <div slot="header" class="clearfix">
                 <span>修改管理员密码</span>
             </div>
-            <el-form :mode="form" ref="form" status-icon :rules="rules">
+            <el-form :model="form" ref="form" status-icon :rules="rules">
                 <el-form-item label="旧密码" prop="oldKey">
                     <el-input type="password" v-model="form.oldKey" placeholder="请输入旧密码" autocomplete="off"></el-input>
                 </el-form-item>
@@ -18,9 +18,9 @@
                 <el-form-item label="再次输入" prop="checkKey">
                     <el-input type="password" v-model="form.checkKey" placeholder="请确认新密码" autocomplete="off"></el-input>
                 </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" size="small">确认</el-button>
-                    <el-button size="small">重输</el-button>
+                <el-form-item class="btn">
+                    <el-button type="primary" size="small" @click="submit('form')">确认</el-button>
+                    <el-button size="small" @click="reset('form')">重输</el-button>
                 </el-form-item>
             </el-form>
         </el-card>
@@ -28,8 +28,17 @@
 </template>
 
 <script>
+import {HOST, ERR_OK} from '@/common/js/config'
+
 export default {
     data () {
+        var checkOldKey = (rule, value, callback) => {
+            if (!value) {
+                return callback(new Error('密码不能为空'))
+            } else {
+                callback()
+            }
+        }
         var validatePass = (rule, value, callback) => {
             if (value === '') {
                 callback(new Error('请输入密码'))
@@ -56,6 +65,9 @@ export default {
                 checkKey: ''
             },
             rules: {
+                oldKey: [
+                    { validator: checkOldKey, trigger: 'blur'}
+                ],
                 newKey: [
                     { validator: validatePass, trigger: 'blur'}
                 ],
@@ -64,6 +76,38 @@ export default {
                 ]
             }
         }
+    },
+    methods: {
+        submit () {
+            const url = `${HOST}/admin`
+            var tmpForm = {}
+            tmpForm.aname = this.$store.state.adminName
+            tmpForm.oldPwd = this.form.oldKey
+            tmpForm.newPwd = this.form.newKey
+            this.$axios.patch(url, tmpForm).then(res => {
+                if (res.data.code === 400) {
+                    this.$message({
+                        message: '旧密码输入不正确',
+                        type: 'warning'
+                    })
+                } else if (res.data.code === 401) {
+                    this.$message({
+                        message: '密码未做修改',
+                        type: 'success'
+                    })
+                } else if (res.data.code === ERR_OK) {
+                    this.$message({
+                        message: '密码修改成功！',
+                        type: 'success'
+                    })
+                }
+            })
+
+            
+        },
+        reset (formName) {
+            this.$refs[formName].resetFields()
+        }
     }
 }
 </script>
@@ -71,7 +115,8 @@ export default {
 <style lang="scss">
 .security {
     .el-card {
-        margin-top: 20px;
+        margin: 0 auto;
+        margin-top: 40px;
         max-width: 500px;
         .el-form {
             .el-form-item {
@@ -81,6 +126,9 @@ export default {
                 .el-input {
                     max-width: 350px;
                 }
+            }
+            .btn {
+                text-align: center;
             }
         }
     }
